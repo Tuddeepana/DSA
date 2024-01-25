fdc6808150a21be606d18e76d1ef629c5752742d
Tuddeepana <tauddeepana@gmail.com>
2024-01-25 10:57:31 +0530

    Array filr update

--- src/ArrayVar.java
+++ src/ArrayVar.java
@@ -1,17 +1,24 @@
 public class ArrayVar {
-    //int i =0; //Instance Variable
+    int i =0; //Instance Variable
     ArrayVar(){
 
         int [] a = new int[5];
 
 
-        for(int i=0; i<a.length; i++){
+        for( i=0; i<a.length; i++){
             int x = a[i]=i+1;
 
             System.out.println("Array :"+ x);
 
         }
 
+        int [] arr = new int[]{1,2,3};
+        for (i=0; i<arr.length; i++){
+            System.out.println("Array 1 "+arr[i]);
+        }
+
     }
+
+
 
 }
