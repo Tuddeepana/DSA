71494914758dcd05fc92654a221826f23300e400
Tuddeepana <tauddeepana@gmail.com>
2024-01-25 10:37:32 +0530

    Variable Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Test Data Structures and Algorithm");
+
+        //Variables
+        new SampleVar();
+
+
+
+
+
     }
 }
---
+++ src/SampleVar.java
@@ -0,0 +1,23 @@
+public class SampleVar {
+
+
+    SampleVar(){
+        int x = 100;
+        int y =50;
+
+        String name = "Phone";
+        boolean a = true;
+
+
+
+        System.out.println("Integer variable :" +x);
+        System.out.println("String variable :"+ name);
+        System.out.println("Boolean Variable" + a);
+
+    }
+
+
+
+
+
+}
